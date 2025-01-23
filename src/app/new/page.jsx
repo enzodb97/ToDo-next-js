c1d7560312860406f9bf3deab29a630c9833400e
Enzo Daniel Bertolusso <151404661+enzodb97@users.noreply.github.com>
2025-01-23 19:54:21 -0300

    frontend put and delete
    Se ha actualizado el fronten donde se terminó la actualización de datos en PUT y hemos agregado ELIMINAR en DELETE

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,128 +1,185 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-"use client"
+function NewPage({ params }) {
+  const [id, setId] = useState(null);
+  const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [state, setState] = useState("Sin iniciar");
+  const [priority, setPriority] = useState("Baja");
+  const [resolvedParams, setResolvedParams] = useState(null);
 
-function NewPage() {
-const onSubmit = async (e)=>{
-    e.preventDefault()
-    const title = e.target.title.value
-    const priority = e.target.priority.value
-    const state = e.target.state.value
-    const description = e.target.description.value
+  useEffect(() => {
+    const fetchParams = async () => {
+      const result = await params;
+      setResolvedParams(result);
+    };
+    fetchParams();
+  }, [params]);
 
-    const response = await fetch('/api/tasks',{
-        method: 'POST',
-        body: JSON.stringify({title, description, priority, state}),
-        headers:{
-            'Content-Type': "application/json"
-        }
-    })
-    const data = await response.json()
-    console.log(data)
-}
-const handleReset = (e) => {
-    e.target.form.reset();
+  useEffect(() => {
+    const fetchData = async () => {
+      if (resolvedParams?.id) {
+        const response = await fetch(`/api/tasks/${resolvedParams.id}`);
+        const data = await response.json();
+        setTitle(data.title);
+        setDescription(data.description);
+        setPriority(data.priority);
+        setState(data.state);
+        setId(resolvedParams.id);
+      }
+    };
+    fetchData();
+  }, [resolvedParams]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (id) {
+      const response = await fetch(`/api/tasks/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({ title, description, state, priority }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+    } else {
+      const response = await fetch("/api/tasks", {
+        method: "POST",
+        body: JSON.stringify({ title, description, priority, state }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+    }
+    router.refresh();
+    router.push("/");
   };
-
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-800">
-    <form onSubmit={onSubmit}>
-      <div className="bg-gray-900 p-8 rounded-lg w-full max-w-2xl">
-        <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div className="sm:col-span-3">
-            <label
-              htmlFor="title"
-              className="block text-sm font-medium text-white"
-            >
-              Nombre de la tarea
-            </label>
-            <div className="mt-2">
-              <input
-                id="title"
-                name="first-name"
-                type="text"
-                autoComplete="given-name"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-600"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-3">
-            <label
-              htmlFor="priority"
-              className="block text-sm font-medium text-white"
-            >
-              Prioridad
-            </label>
-            <div className="mt-2">
-              <select
-                id="priority"
-                name="priority"
-                className="block w-full rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-600"
+      <form onSubmit={onSubmit}>
+        <div className="bg-gray-900 p-8 rounded-lg w-full max-w-2xl">
+          <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="title"
+                className="block text-sm font-medium text-white"
               >
-                <option>Baja</option>
-                <option>Media</option>
-                <option>Alta</option>
-                <option>Urgente</option>
-              </select>
+                Nombre de la tarea
+              </label>
+              <div className="mt-2">
+                <input
+                  id="title"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-600"
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title}
+                />
+              </div>
             </div>
-          </div>
-          <div className="sm:col-span-3">
-            <label
-              htmlFor="state"
-              className="block text-sm font-medium text-white"
-            >
-              Estado
-            </label>
-            <div className="mt-2">
-              <select
-                id="state"
-                name="state"
-                className="block w-full rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-600"
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="priority"
+                className="block text-sm font-medium text-white"
               >
-                <option>Sin iniciar</option>
-                <option>Iniciado</option>
-                <option>En espera</option>
-                <option>En conflicto</option>
-                <option>Terminado</option>
-              </select>
+                Prioridad
+              </label>
+              <div className="mt-2">
+                <select
+                  id="priority"
+                  name="priority"
+                  className="block w-full rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-600"
+                  onChange={(e) => setPriority(e.target.value)}
+                  value={priority}
+                >
+                  <option>Baja</option>
+                  <option>Media</option>
+                  <option>Alta</option>
+                  <option>Urgente</option>
+                </select>
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="state"
+                className="block text-sm font-medium text-white"
+              >
+                Estado
+              </label>
+              <div className="mt-2">
+                <select
+                  id="state"
+                  name="state"
+                  className="block w-full rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-600"
+                  onChange={(e) => setState(e.target.value)}
+                  value={state}
+                >
+                  <option>Sin iniciar</option>
+                  <option>Iniciado</option>
+                  <option>En espera</option>
+                  <option>En conflicto</option>
+                  <option>Terminado</option>
+                </select>
+              </div>
+            </div>
+            <div className="col-span-full">
+              <label
+                htmlFor="about"
+                className="block text-sm font-medium text-white"
+              >
+                Sobre la tarea
+              </label>
+              <div className="mt-2">
+                <textarea
+                  name="about"
+                  id="description"
+                  rows="3"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-600"
+                  onChange={(e) => setDescription(e.target.value)}
+                  value={description}
+                ></textarea>
+              </div>
+              <p className="mt-3 text-sm text-gray-400">
+                Escribe lo más detallado posible la tarea.
+              </p>
             </div>
           </div>
-          <div className="col-span-full">
-            <label
-              htmlFor="about"
-              className="block text-sm font-medium text-white"
+          <hr />
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            {resolvedParams?.id && (
+              <button
+                type="button"
+                className="text-sm/6 font-semibold text-white"
+                onClick={async () => {
+                  const response = await fetch(`/api/tasks/${resolvedParams.id}`, {
+                    method: "DELETE",
+                  });
+                  const data = await response.json();
+                  router.refresh();
+                  router.push("/");
+                }}
+              >
+                Eliminar
+              </button>
+            )}
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sobre la tarea
-            </label>
-            <div className="mt-2">
-              <textarea
-                name="about"
-                id="description"
-                rows="3"
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-600"
-              ></textarea>
-            </div>
-            <p className="mt-3 text-sm text-gray-400">
-              Escribe lo más detallado posible la tarea.
-            </p>
+              Guardar
+            </button>
           </div>
         </div>
-        <hr />
-        <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" className="text-sm/6 font-semibold text-white" onClick={handleReset}>
-          Borrar
-        </button>
-        <button
-          type="submit"
-          
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Guardar
-        </button>
-      </div>
-      </div>
       </form>
     </div>
   );
 }
+
 export default NewPage;
+  
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,50 +1,68 @@
-function TaskCard  ({task}) {
+"use client";
+import { useRouter } from "next/navigation";
+
+function TaskCard({ task }) {
+  const router = useRouter();
   return (
-    <div className="bg-white p-5 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
-    <h3 className="text-xl font-semibold text-gray-800">{task.title}</h3>
-    <div className="flex justify-between items-center mt-2 mb-4">
-      <span className={`text-sm font-medium px-2 py-1 rounded-full ${getPriorityClass(task.priority)}`}>
-        {task.priority}
-      </span>
-      <span className={`text-sm font-medium px-2 py-1 rounded-full ${getStateClass(task.state)}`}>
-        {task.state}
-      </span>
+    <div className="bg-white p-5 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 hover:cursor-pointer"
+    onClick={()=>{
+        router.push('/tasks/edit/'+ task.id)
+    }}
+    >
+      <h3 className="text-xl font-semibold text-gray-800">{task.title}</h3>
+      <div className="flex justify-between items-center mt-2 mb-4">
+        <span
+          className={`text-sm font-medium px-2 py-1 rounded-full ${getPriorityClass(
+            task.priority
+          )}`}
+        >
+          {task.priority}
+        </span>
+        <span
+          className={`text-sm font-medium px-2 py-1 rounded-full ${getStateClass(
+            task.state
+          )}`}
+        >
+          {task.state}
+        </span>
+      </div>
+      <p className="text-gray-600">{task.description}</p>
+      <p className="text-gray-400 mt-2 text-sm">
+        {new Date(task.createdAt).toLocaleDateString()}
+      </p>
     </div>
-    <p className="text-gray-600">{task.description}</p>
-    <p className="text-gray-400 mt-2 text-sm">{new Date(task.createdAt).toLocaleDateString()}</p>
-  </div>
-  )
+  );
 }
 function getPriorityClass(priority) {
-    switch (priority) {
-      case 'Baja':
-        return 'bg-green-200 text-green-800';
-      case 'Media':
-        return 'bg-yellow-200 text-yellow-800';
-      case 'Alta':
-        return 'bg-orange-200 text-orange-800';
-      case 'Urgente':
-        return 'bg-red-200 text-red-800';
-      default:
-        return 'bg-gray-200 text-gray-800';
-    }
+  switch (priority) {
+    case "Baja":
+      return "bg-green-200 text-green-800";
+    case "Media":
+      return "bg-yellow-200 text-yellow-800";
+    case "Alta":
+      return "bg-orange-200 text-orange-800";
+    case "Urgente":
+      return "bg-red-200 text-red-800";
+    default:
+      return "bg-gray-200 text-gray-800";
   }
-  
-  function getStateClass(state) {
-    switch (state) {
-      case 'Sin iniciar':
-        return 'bg-gray-200 text-gray-800';
-      case 'Iniciado':
-        return 'bg-blue-200 text-blue-800';
-      case 'En espera':
-        return 'bg-yellow-200 text-yellow-800';
-      case 'En conflicto':
-        return 'bg-red-200 text-red-800';
-      case 'Terminado':
-        return 'bg-green-200 text-green-800';
-      default:
-        return 'bg-gray-200 text-gray-800';
-    }
+}
+
+function getStateClass(state) {
+  switch (state) {
+    case "Sin iniciar":
+      return "bg-gray-200 text-gray-800";
+    case "Iniciado":
+      return "bg-blue-200 text-blue-800";
+    case "En espera":
+      return "bg-yellow-200 text-yellow-800";
+    case "En conflicto":
+      return "bg-red-200 text-red-800";
+    case "Terminado":
+      return "bg-green-200 text-green-800";
+    default:
+      return "bg-gray-200 text-gray-800";
   }
-  
-export default TaskCard
+}
+
+export default TaskCard;
