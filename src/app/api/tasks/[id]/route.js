7c5efeba386d3c0cd709a09d5ab14e3861632f5d
Enzo Daniel Bertolusso <151404661+enzodb97@users.noreply.github.com>
2025-01-22 23:52:02 -0300

    actulizando los metodos GET y DELETE
    Se hicieron pruebas de correcto funcionamiento, ambos métodos están ejecutándose con éxito.

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,14 +1,30 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
-export function GET(request, {params}){
-
-    return NextResponse.json("obteniendo tarea" + params.id)
+export async function GET(request, { params }) {
+  const task = await prisma.task.findUnique({
+    where: {
+      id: Number(params.id),
+    },
+  });
+  return NextResponse.json(task);
 }
-export function PUT(request, {params}){
 
-    return NextResponse.json("Actualizar tarea" + params.id)
+export function PUT(request, { params }) {
+  return NextResponse.json("Actualizar tarea" + params.id);
 }
-export function DELETE(request, {params}){
 
-    return NextResponse.json("Eliminar tarea" + params.id)
+export async function DELETE(request, { params }) {
+  try {
+    const task = await prisma.task.delete({
+      where: {
+        id: Number(params.id),
+      },
+    });
+    return NextResponse.json(
+      "Tarea con la id" + params.id + " eliminada con éxito"
+    );
+  } catch (error) {
+    return NextResponse.json(error.message);
+  }
 }
