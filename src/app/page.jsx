e212618ef5a1d4a95810f5ae02c47d0664e43b14
Enzo Daniel Bertolusso <151404661+enzodb97@users.noreply.github.com>
2025-01-23 13:22:48 -0300

    frontend listamos las tareas
    listamos en la pagina principal todas las tareas que se encuentran en la bbdd

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,26 @@
-function HomePage() {
-  return (
-    <div>
-      <p>Tareas</p>
-      </div>
-  )
+import { prisma } from "@/libs/prisma";
+import TaskCard from "@/components/TaskCard";
+
+async function loadTasks() {
+  return await prisma.task.findMany();
 }
 
-export default HomePage
+async function HomePage() {
+  const tasks = await loadTasks();
+  console.log(tasks);
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <div className="container mx-auto">
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {tasks.map((task) => (
+           <TaskCard task={task} key={task.id}/>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default HomePage;
